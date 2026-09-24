5fa928c5e58192c5014f2c62896d49061506bebc
cardillan <122014763+cardillan@users.noreply.github.com>
2026-09-24 09:46:57 +0200

    Release 0.10.11

--- src/cardillan/mlogassertions/MlogAssertions.java
+++ src/cardillan/mlogassertions/MlogAssertions.java
@@ -1,11 +1,14 @@
 package cardillan.mlogassertions;
 
+import arc.Core;
 import arc.Events;
 import cardillan.mlogassertions.logic.AssertLogic;
 import cardillan.mlogassertions.ui.Assertions;
 import cardillan.mlogassertions.ui.LogicDialogAddon;
 import cardillan.mlogassertions.ui.MemoryConfiguration;
 import cardillan.mlogassertions.ui.MemoryVars;
+import mindustry.Vars;
+import mindustry.core.Version;
 import mindustry.game.EventType;
 import mindustry.mod.Mod;
 
@@ -13,6 +16,15 @@ public class MlogAssertions extends Mod {
 
     public MlogAssertions() {
         Events.on(EventType.ClientLoadEvent.class, e -> {
+            if (Version.build >= 160) {
+                Core.app.post(() -> Vars.ui.showErrorMessage(
+                        """
+                                This release of MlogAssertions doesn't support Mindustry 160 or higher.
+                                Please update your mod to a compatible version using the Mod Browser.
+                                The mod remains inactive to prevent crashes in the game."""));
+                return;
+            }
+
             Settings.init();
 
             LogicDialogAddon.init();
